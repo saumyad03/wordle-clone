b547ba173c0a67e14712b3d07af387c8d2049acd
saumyad03 <saumyad03@gmail.com>
2022-05-19 21:34:20 -0400

    Fixes minor bug involving excessive yellow boxes when they should be gray

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const words = ["ABUSE","ADULT","AGENT","ANGER","APPLE","AWARD","BASIS","BEACH","BIRTH","BLOCK","BLOOD","BOARD","BRAIN","BREAD","BREAK","BROWN","BUYER","CAUSE","CHAIN","CHAIR","CHEST","CHIEF","CHILD","CHINA","CLAIM","CLASS","CLOCK","COACH","COAST","COURT","COVER","CREAM","CRIME","CROSS","CROWD","CROWN","CYCLE","DANCE","DEATH","DEPTH","DOUBT","DRAFT","DRAMA","DREAM","DRESS","DRINK","DRIVE","EARTH","ENEMY","ENTRY","ERROR","EVENT","FAITH","FAULT","FIELD","FIGHT","FINAL","FLOOR","FOCUS","FORCE","FRAME","FRANK","FRONT","FRUIT","GLASS","GRANT","GRASS","GREEN","GROUP","GUIDE","HEART","HENRY","HORSE","HOTEL","HOUSE","IMAGE","INDEX","INPUT","ISSUE","JAPAN","JONES","JUDGE","KNIFE","LAURA","LAYER","LEVEL","LEWIS","LIGHT","LIMIT","LUNCH","MAJOR","MARCH","MATCH","METAL","MODEL","MONEY","MONTH","MOTOR","MOUTH","MUSIC","NIGHT","NOISE","NORTH","NOVEL","NURSE","OFFER","ORDER","OTHER","OWNER","PANEL","PAPER","PARTY","PEACE","PETER","PHASE","PHONE","PIECE","PILOT","PITCH","PLACE","PLANE","PLANT","PLATE","POINT","POUND","POWER","PRESS","PRICE","PRIDE","PRIZE","PROOF","QUEEN","RADIO","RANGE","RATIO","REPLY","RIGHT","RIVER","ROUND","ROUTE","RUGBY","SCALE","SCENE","SCOPE","SCORE","SENSE","SHAPE","SHARE","SHEEP","SHEET","SHIFT","SHIRT","SHOCK","SIGHT","SIMON","SKILL","SLEEP","SMILE","SMITH","SMOKE","SOUND","SOUTH","SPACE","SPEED","SPITE","SPORT","SQUAD","STAFF","STAGE","START","STATE","STEAM","STEEL","STOCK","STONE","STORE","STUDY","STUFF","STYLE","SUGAR","TABLE","TASTE","TERRY","THEME","THING","TITLE","TOTAL","TOUCH","TOWER","TRACK","TRADE","TRAIN","TREND","TRIAL","TRUST","TRUTH","UNCLE","UNION","UNITY","VALUE","VIDEO","VISIT","VOICE","WASTE","WATCH","WATER","WHILE","WHITE","WHOLE","WOMAN","WORLD","YOUTH","ADMIT","ADOPT","AGREE","ALLOW","ALTER","APPLY","ARGUE","ARISE","AVOID","BEGIN","BLAME","BREAK","BRING","BUILD","BURST","CARRY","CATCH","CAUSE","CHECK","CLAIM","CLEAN","CLEAR","CLIMB","CLOSE","COUNT","COVER","CROSS","DANCE","DOUBT","DRINK","DRIVE","ENJOY","ENTER","EXIST","FIGHT","FOCUS","FORCE","GUESS","IMPLY","ISSUE","JUDGE","LAUGH","LEARN","LEAVE","LIMIT","MARRY","MATCH","OCCUR","OFFER","ORDER","PHONE","PLACE","POINT","PRESS","PROVE","RAISE","REACH","REFER","RELAX","SERVE","SHALL","SHARE","SHIFT","SHOOT","SLEEP","SOLVE","SOUND","SPEAK","SPEND","SPLIT","STAND","START","STATE","STICK","STUDY","TEACH","THANK","THINK","THROW","TOUCH","TRAIN","TREAT","TRUST","VISIT","VOICE","WASTE","WATCH","WORRY","WOULD","WRITE","ABOVE","ACUTE","ALIVE","ALONE","ANGRY","AWARE","AWFUL","BASIC","BLACK","BLIND","BRAVE","BRIEF","BROAD","BROWN","CHEAP","CHIEF","CIVIL","CLEAN","CLEAR","CLOSE","CRAZY","DAILY","DIRTY","EARLY","EMPTY","EQUAL","EXACT","EXTRA","FAINT","FALSE","FIFTH","FINAL","FIRST","FRESH","FRONT","FUNNY","GIANT","GRAND","GREAT","GREEN","GROSS","HAPPY","HARSH","HEAVY","HUMAN","IDEAL","INNER","JOINT","LARGE","LEGAL","LEVEL","LIGHT","LOCAL","LOOSE","LUCKY","MAGIC","MAJOR","MINOR","MORAL","NAKED","NASTY","NAVAL","OTHER","OUTER","PLAIN","PRIME","PRIOR","PROUD","QUICK","QUIET","RAPID","READY","RIGHT","ROMAN","ROUGH","ROUND","ROYAL","RURAL","SHARP","SHEER","SHORT","SILLY","SIXTH","SMALL","SMART","SOLID","SORRY","SPARE","STEEP","STILL","SUPER","SWEET","THICK","THIRD","TIGHT","TOTAL","TOUGH","UPPER","UPSET","URBAN","USUAL","VAGUE","VALID","VITAL","WHITE","WHOLE","WRONG","YOUNG"];
 let word = words[Math.floor(Math.random() * words.length)];
 let wordArr = createWordArr(word);
+let wordMap = createWordMap(wordArr);
 let guessArr = [];
 guesses = 0;
 //selects all boxes
@@ -52,18 +53,30 @@ newGameBtn.addEventListener('click', function(event) {
 })
 //could pass parameter for guess
 function checkGuess() {
-    //checks every guessed letter
+    //clone hashmap
+    let cloneMap = createWordMap(wordArr);
+    //handle greens
     for (let i = 0; i < guessArr.length; i++) {
-        //if letter is in right spot
         if (guessArr[i] == wordArr[i]) {
             currBoxes[i].style.backgroundColor = 'green';
-        //if letter in word but not right spot
-        } else if (wordArr.includes(guessArr[i])) {
+            let occ = cloneMap.get(guessArr[i]);
+            occ -= 1;
+            cloneMap.set(guessArr[i], occ);
+        }
+    }
+    //handle yellows and grays
+    for (let i = 0; i < guessArr.length; i++) {
+        //if green skip
+        if (guessArr[i] == wordArr[i]) {
+            continue;
+        }
+        //handles yellows and grays
+        if (cloneMap.has(guessArr[i]) && cloneMap.get(guessArr[i]) > 0) {
             currBoxes[i].style.backgroundColor = 'yellow';
-        //if letter not in word
         } else {
             currBoxes[i].style.backgroundColor = 'gray';
         }
+
     }
 }
 function compareWordArrays(guess, word) {
@@ -94,4 +107,19 @@ function createWordArr(w) {
         arr.push(w[i]);
     }
     return arr;
+}
+function createWordMap(wArr) {
+    let letterMap = new Map();
+    for (let i = 0; i < wArr.length; i++) {
+        letter = wArr[i];
+        if (letterMap.has(letter)) {
+            let occ = letterMap.get(letter);
+            occ += 1;
+            letterMap.set(letter, occ);
+        } else {
+            letterMap.set(letter, 1)
+        }
+    }
+    return letterMap;
+
 }
